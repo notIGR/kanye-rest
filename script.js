d41517099e45f13f39f8a0ad8cc19a220a27fcda
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-16 10:48:34 -0400

    less code the better

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 const API_URL = "https://api.kanye.rest";
 alert('hello world');
 async function main() {
-	const data = await console.log('hello world')	//command goes here
+	const data = await console.log()	//command goes here
 }
